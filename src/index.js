2e72abce7873bef915042a5d0e119719a9a49fd6
Ynno <ravinnpolestico@gmail.com>
2024-08-24 00:51:30 +0800

    This is it pancit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,17 @@
-// Original
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-
-// const myFirstElement = <h1>I Miss You Baby!</h1>
-// const mySecondElement = <h1>I Love You Pau!</h1>
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(myFirstElement);
-// root.render(mySecondElement);
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const myFirstElement = <h1>I Miss You Baby!</h1>;
-const mySecondElement = <h1>I Love You Pau!</h1>;
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>
-    {myFirstElement}
-    {mySecondElement}
-  </div>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
